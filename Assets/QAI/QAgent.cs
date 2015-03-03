2aa95711bc24d3deaceaafd101871208467fca6f
Nikolaj Korsbæk Falsted <nikolajfalsted@gmail.com>
2015-03-03 13:10:09 +0100

    Actions use name instead of index, as identifiers

--- Assets/QAI/QAgent.cs
+++ Assets/QAI/QAgent.cs
@@ -9,13 +9,12 @@ public interface QAgent {
 
 public static class QAgentExtension {
     public static QAction[] GetQActions(this QAgent agent) {
-        int i = 0;
         return
             (from m in agent.GetType().GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
             where m.GetCustomAttributes(typeof(QBehavior), true).Length > 0
-            select new QAction {
-                ActionIndex = i++,
-                Action = (Action)Delegate.CreateDelegate(typeof(Action), agent, m.Name)
-            }).ToArray();
+            select new QAction(
+                m.Name,
+                (Action)Delegate.CreateDelegate(typeof(Action), agent, m.Name)
+            )).ToArray();
     }
 }
