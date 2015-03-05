27bacae3e2f216e5eb2e3a5b9394ad9836bcfd6d
Jacob Fischer <jaco@itu.dk>
2015-03-05 09:20:38 +0100

    Time to QPredicate that QBehavior

--- Assets/QAI/QBehavior.cs
+++ Assets/QAI/QBehavior.cs
@@ -1,5 +1,32 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
 
 [AttributeUsage(AttributeTargets.Method)]
-class QBehavior : Attribute {
+public class QBehavior : Attribute {
+
+    private bool conditional = false;
+    private string predicate;
+
+    public QBehavior() {
+    }
+
+    public QBehavior(string predicate) {
+        conditional = true;
+        this.predicate = predicate;
+    }
+
+    public QAction ObtainAction(QAgent agent, string name) {
+        var p = agent.GetType().GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+            .Where(m => m.Name == predicate).FirstOrDefault();
+        if (p == null)
+            throw new Exception("Predicate method " + predicate + " does not exist.");
+        if (p.GetCustomAttributes(typeof(QPredicate), true).Length == 0)
+            throw new Exception("Predicate method " + predicate + " is not properly annotated as a QPredicate.");
+        return new QAction(
+            name,
+            (Action)Delegate.CreateDelegate(typeof(Action), agent, name),
+            conditional ? (QPredicate.Basic)Delegate.CreateDelegate(typeof(QPredicate.Basic), agent, predicate) : null
+        );
+    }
 }
--- Assets/QAI/QLearning.cs
+++ Assets/QAI/QLearning.cs
@@ -10,6 +10,8 @@ using UnityEngine;
 using Random = System.Random;
 
 class QLearning {
+    private delegate double param(int i);
+
     private readonly QTable _QTable;
     private readonly QAgent _agent;
     private readonly Random _rng;
@@ -21,7 +23,7 @@ class QLearning {
         _rng = new Random();
         
         if (table == null) {
-            _QTable = new QTable();
+            _QTable = new QTable(1); // Default reward of 1.0
             _QTable.Load(QTablePath);
         } else {
             _QTable = table;
@@ -32,26 +34,27 @@ class QLearning {
         return _QTable.Query(s, a);
     }
 
-    private const double Epsilon = 0.8;
-    private QAction EpsilonGreedy(QState s, QAction[] actions) {
-        if (_rng.NextDouble() > Epsilon) return actions[_rng.Next(actions.Length)];
+    private param eps = t => 0.8;
+    private QAction EpsilonGreedy(QState s, QAction[] actions, int t) {
+        if (_rng.NextDouble() < eps(t)) return actions[_rng.Next(actions.Length)];
         var ordered = actions.Select(a => new {A = a, Q = Q(s, a)}).OrderByDescending(x => x.Q);
         var highest = ordered.First().Q;
         var highArray = ordered.Where(x => x.Q == highest).ToArray();
         return highArray[_rng.Next(highArray.Length)].A;
     }
 
-    private const double Discount = 0.5;
+    private param discount = t => 1.0 / t;
+    private param stepsize = t => 1.0 / t;
     public IEnumerator Learn(int iteration) {
         var t = 1;
         var s = _agent.GetState();
         var actions = _agent.GetQActions();
         while (!s.IsTerminal) {
-            var a = EpsilonGreedy(s, actions);
+            var a = EpsilonGreedy(s, actions.Where(ac => ac.IsValid()).ToArray(), t);
             a.Action.Invoke();
             var s0 = _agent.GetState();
             var a0max = actions.Max(a0 => Q(s0, a0));
-            var v = Q(s, a) + 1.0/t * (s0.Reward + Discount * a0max - Q(s, a));
+            var v = Q(s, a) + stepsize(t) * (s0.Reward + discount(t) * a0max - Q(s, a));
             _QTable.Add(s,a,v);
             s = s0;
             t++;
