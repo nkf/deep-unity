dc720ca0cccf13d9d67a80f942944a660851d5b7
Mathias Vielwerth <onimishra@gmail.com>
2015-03-10 09:49:46 +0100

    Option window. Intermediary commit to merge with the base.

---
+++ Assets/QAI/QAIOptionWindow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Linq;
+
+public class QAIOptionWindow : EditorWindow {
+	private bool _imitation;
+	private bool _show = true;
+
+	// Add menu named "My Window" to the Window menu
+	[MenuItem ("QAI/Options")]
+	static void Init () {
+		// Get existing open window or if none, make a new one:
+		QAIOptionWindow window = (QAIOptionWindow)EditorWindow.GetWindow (typeof (QAIOptionWindow));
+		window._imitation = GameObject.FindObjectsOfType<QAI>().All(q => q.ImitationProcess);
+		window.Show();
+	}
+
+	void OnGUI() {
+		if(_show = EditorGUILayout.Foldout(_show, "Imitation Learning")) {	
+			_imitation = EditorGUILayout.Toggle("Learn on play", _imitation);
+		}
+		foreach(var ai in GameObject.FindObjectsOfType<QAI>()) {
+			ai.ImitationProcess = _imitation;
+		}
+	}
+}
+
