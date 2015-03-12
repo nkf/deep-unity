f3bdbcca354975da75070d0f737f67019c8e1eef
Mathias Vielwerth <onimishra@gmail.com>
2015-03-12 13:59:19 +0100

    SARS!

---
+++ Assets/QAI/SARS.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public struct SARS {
+	public QState state;
+	public QState nextState;
+	public QAction action;
+	public double reward;
+	
+	public SARS(QState s, QAction a, double r, QState s0) {
+		state = s; action = a; reward = r; nextState = s0;
+	}
+}
