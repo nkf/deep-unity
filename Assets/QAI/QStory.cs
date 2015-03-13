0103bae6b0002164724b2c84ccb1da2992ebf118
Nikolaj Korsbæk Falsted <nikolajfalsted@gmail.com>
2015-03-13 09:44:55 +0100

    Forgot to commit the actual story class

---
+++ Assets/QAI/QStory.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+public class QStory {
+    public string ScenePath { get; set; }
+    public string SceneName {
+        get {
+            return Path.GetFileNameWithoutExtension(ScenePath);
+        }
+    }
+    public int Iterations { get; set; }
+
+
+}
