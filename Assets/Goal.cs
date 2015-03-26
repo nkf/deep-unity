93b9695a0b9b1fb71cbe8d3c4da61b0421a567aa
Jacob Fischer <jaco@itu.dk>
2015-03-26 14:28:54 +0100

    U_1 parially solved with arbitrary goal positions.

--- Assets/Goal.cs
+++ Assets/Goal.cs
@@ -6,13 +6,14 @@ public class Goal : MonoBehaviour {
     
     private static int _index = 0;
     public List<Vector3> GoalPositions;
+    public bool arbitrarySpawn;
     
     public static Goal Instance;
     public static Vector3 Position;
     public static int[] State;
 
     public void Awake() {
-        if (GoalPositions.Count > 0) {
+        if (GoalPositions.Count > 0 && arbitrarySpawn) {
             transform.position = GoalPositions[_index];
             _index++;
             if (_index >= GoalPositions.Count) _index = 0;
