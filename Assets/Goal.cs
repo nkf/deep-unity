864412e6b84aa820146bde5cab5e6bf22d19ed42
Nikolaj Korsbæk Falsted <nikolajfalsted@gmail.com>
2015-03-26 14:09:37 +0100

    Code to change the position of goal in each iteration

--- Assets/Goal.cs
+++ Assets/Goal.cs
@@ -1,13 +1,23 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using System.Collections;
 
 public class Goal : MonoBehaviour {
-
+    
+    private static int _index = 0;
+    public List<Vector3> GoalPositions;
+    
     public static Goal Instance;
-
     public static Vector3 Position;
     public static int[] State;
-    void Awake() {
+
+    public void Awake() {
+        if (GoalPositions.Count > 0) {
+            transform.position = GoalPositions[_index];
+            _index++;
+            if (_index >= GoalPositions.Count) _index = 0;
+        }
+
         Instance = this;
         Position = transform.position;
         State = new[] {Mathf.RoundToInt(Position.x), 1, Mathf.RoundToInt(Position.z)};
