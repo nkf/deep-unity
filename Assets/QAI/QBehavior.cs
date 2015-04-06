e1f52ad621582380070e9a9268bfe1b9fdc32d0e
Nikolaj K. Falsted <nikolajfalsted@gmail.com>
2015-04-06 19:40:57 +0200

    Fixed QBehavior failing if no predicate was specified

--- Assets/QAI/QBehavior.cs
+++ Assets/QAI/QBehavior.cs
@@ -5,28 +5,32 @@ using System.Reflection;
 [AttributeUsage(AttributeTargets.Method)]
 public class QBehavior : Attribute {
 
-    private bool conditional = false;
-    private string predicate;
+    private readonly bool _conditional = false;
+    private readonly string _predicate;
 
     public QBehavior() {
     }
 
     public QBehavior(string predicate) {
-        conditional = true;
-        this.predicate = predicate;
+        _conditional = true;
+        _predicate = predicate;
     }
 
     public QAction ObtainAction(QAgent agent, string name) {
-        var p = agent.GetType().GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
-            .Where(m => m.Name == predicate).FirstOrDefault();
-        if (p == default(MethodInfo))
-            throw new Exception("Predicate method " + predicate + " does not exist.");
-        if (p.GetCustomAttributes(typeof(QPredicate), true).Length == 0)
-            throw new Exception("Predicate method " + predicate + " is not properly annotated as a QPredicate.");
+        if (_conditional) {
+            var p =
+                agent.GetType()
+                    .GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+                    .FirstOrDefault(m => m.Name == _predicate);
+            if (p == default(MethodInfo))
+                throw new Exception("Predicate method " + _predicate + " does not exist.");
+            if (p.GetCustomAttributes(typeof (QPredicate), true).Length == 0)
+                throw new Exception("Predicate method " + _predicate + " is not properly annotated as a QPredicate.");
+        }
         return new QAction(
             name,
             (Action)Delegate.CreateDelegate(typeof(Action), agent, name),
-            conditional ? (QPredicate.Basic)Delegate.CreateDelegate(typeof(QPredicate.Basic), agent, predicate) : null
+            _conditional ? (QPredicate.Basic)Delegate.CreateDelegate(typeof(QPredicate.Basic), agent, _predicate) : null
         );
     }
 }
