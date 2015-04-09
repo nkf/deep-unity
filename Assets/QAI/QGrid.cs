6b73c1c05f44316170acdc96f5f9a69b1d5eeb11
Nikolaj Korsbæk Falsted <nikolajfalsted@gmail.com>
2015-04-09 10:57:23 +0200

    Grid is now 3d and uses the bounds class

--- Assets/QAI/QGrid.cs
+++ Assets/QAI/QGrid.cs
@@ -4,62 +4,91 @@ using UnityEngine;
 public class QGrid {
     public int Width { get; private set; } //x
     public int Height { get; private set; } //y
+    public int Depth { get; private set; } //z
     public float ResolutionX { get; private set; }
     public float ResolutionY { get; private set; }
+    public float ResolutionZ { get; private set; }
 
     public readonly double[] Grid;
 
-    public double this[int x, int y] {
+    public double this[int x, int y, int z] {
         get {
-            return Grid[x*Width + y];
+            return Grid[x + Width * (y + Height * z)];
         }
         set {
-            Grid[x*Width + y] = value;
+            Grid[x + Width * (y + Height * z)] = value;
         }
     }
 
-    public QGrid(int height, int width, float resolution) {
-        Init(height,width,resolution,resolution);
-        Grid = new double[Width * Height];
+    public QGrid(int height, int width, int depth, float resolution) {
+        Init(height, width, depth,resolution, resolution, resolution);
+        Grid = new double[Width * Height * Depth];
     }
 
-    public QGrid(int height, int width, float resolutionX, float resolutionY) {
-        Init(height,width,resolutionX,resolutionY);
-        Grid = new double[Width * Height];
+    public QGrid(int height, int width, int depth, float resolutionX, float resolutionY, float resolutionZ) {
+        Init(height,width,depth,resolutionX,resolutionY, resolutionZ);
+        Grid = new double[Width * Height * Depth];
     }
 
-    private void Init(int height, int width, float resolutionX, float resolutionY) {
+    private void Init(int height, int width, int depth, float resolutionX, float resolutionY, float resolutionZ) {
         Height = height;
         Width = width;
+        Depth = depth;
         ResolutionX = resolutionX;
         ResolutionY = resolutionY;
+        ResolutionZ = resolutionZ;
     }
 
-    public void Populate(Vector2 center, Func<Vector2, double> populator) {
-        Iterate(center, (x, y, p) => this[x,y] = populator(p));
+    public void Populate(Vector3 center, Func<Bounds, double> populator) {
+        Iterate(center, (x, y, z, p) => this[x,y,z] = populator(p));
     }
 
-    public void DebugDraw(Vector2 center, Func<double, Color> colorFunc = null) {
-        Iterate(center, (x, y, p) => {
-            var nw = new Vector3(p.x - ResolutionX/2, p.y - ResolutionY/2);
-            var ne = new Vector3(p.x + ResolutionX/2, p.y - ResolutionY/2);
-            var sw = new Vector3(p.x - ResolutionX/2, p.y + ResolutionY/2);
-            var se = new Vector3(p.x + ResolutionX/2, p.y + ResolutionY/2);
-            var c = colorFunc == null ? Color.white : colorFunc(this[x,y]);
-            Debug.DrawLine(nw, ne, c);
-            Debug.DrawLine(ne, se, c);
-            Debug.DrawLine(se, sw, c);
-            Debug.DrawLine(sw, nw, c);
+    public void DebugDraw(Vector3 center, Func<double, Color> colorFunc = null) {
+        Iterate(center, (x, y, z, b) => {
+            var c = b.center;
+            var rX = ResolutionX/2; var rY = ResolutionY/2; var rZ = ResolutionZ/2;
+            var aaa = new Vector3(c.x - rX, c.y - rY, c.z - rZ);
+            var baa = new Vector3(c.x + rX, c.y - rY, c.z - rZ);
+            var aba = new Vector3(c.x - rX, c.y + rY, c.z - rZ);
+            var bba = new Vector3(c.x + rX, c.y + rY, c.z - rZ);
+            var aab = new Vector3(c.x - rX, c.y - rY, c.z + rZ);
+            var bab = new Vector3(c.x + rX, c.y - rY, c.z + rZ);
+            var abb = new Vector3(c.x - rX, c.y + rY, c.z + rZ);
+            var bbb = new Vector3(c.x + rX, c.y + rY, c.z + rZ);
+            var color = colorFunc == null ? Color.white : colorFunc(this[x,y,z]);
+
+            //The first four
+            Debug.DrawLine(aaa, baa, color);
+            Debug.DrawLine(baa, bba, color);
+            Debug.DrawLine(bba, aba, color);
+            Debug.DrawLine(aba, aaa, color);
+
+            //The second four
+            Debug.DrawLine(aab, bab, color);
+            Debug.DrawLine(bab, bbb, color);
+            Debug.DrawLine(bbb, abb, color);
+            Debug.DrawLine(abb, aab, color);
+
+            //The connection between the two layers
+            Debug.DrawLine(aaa, aab, color);
+            Debug.DrawLine(baa, bab, color);
+            Debug.DrawLine(aba, abb, color);
+            Debug.DrawLine(bba, bbb, color);
         });
     }
 
 
-    private void Iterate(Vector2 center, Action<int,int,Vector2> f) {
+    private void Iterate(Vector3 center, Action<int,int,int,Bounds> f) {
         var left = center.x - ((Width/2f)*ResolutionX - ResolutionX/2);
         var top = center.y - ((Height/2f)*ResolutionY - ResolutionY/2);
+        var front = center.z -((Depth/2f)*ResolutionZ - ResolutionZ/2);
+        var size = new Vector3(ResolutionX, ResolutionY, ResolutionZ);
         for (var x = 0; x < Width; x++) {
             for (var y = 0; y < Height; y++) {
-                f(x, y, new Vector2(left + x * ResolutionX, top + y * ResolutionY));
+                for (var z = 0; z < Depth; z++) {
+                    var c = new Vector3(left + x * ResolutionX, top + y * ResolutionY, front + z * ResolutionZ);
+                    f(x, y, z, new Bounds(c, size));
+                }
             }
         }
     }
