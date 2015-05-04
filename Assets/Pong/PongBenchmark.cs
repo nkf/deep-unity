840c89490575f1e95b28dd75625eb911a142da32
Nikolaj Korsbæk Falsted <nikolajfalsted@gmail.com>
2015-05-04 10:45:38 +0200

    Added class to benchmark pong

---
+++ Assets/Pong/PongBenchmark.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using UnityEngine;
+
+public class PongBenchmark : QTester {
+
+    private float y = -1;
+    public override bool SetupNextTest(QAgent agent) {
+        FindObjectOfType<PongBall>().Reset(new Vector2(-1, y));
+        y += 0.2f;
+        return y <= 1;
+    }
+
+    public override void OnActionTaken(QAgent agent, SARS sars) {
+        
+    }
+
+    readonly List<double> _rewards = new List<double>(); 
+    public override void OnTestComplete(double reward) {
+        _rewards.Add(reward);
+        Debug.Log("Run Complete");
+    }
+
+    public override void OnRunComplete() {
+        Debug.Log(string.Join(",", _rewards.Select(d => d.ToString(CultureInfo.InvariantCulture)).ToArray()));
+    }
+}
