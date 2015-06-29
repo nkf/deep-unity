eab0fe53cdf893d4278ab3340f411f89a4d963dd
Mathias Vielwerth <onimishra@gmail.com>
2015-06-29 17:34:59 +0200

    Fix

--- Assets/QAI/Visualizer/NetworkVisualizer.cs
+++ Assets/QAI/Visualizer/NetworkVisualizer.cs
@@ -25,7 +25,7 @@ namespace QAI.Visualizer {
 		
 		// Update is called once per frame
 		void OnGUI () {
-			foreach(var l in _layers) l.Update();
+			foreach(var l in _convLayers) l.Update();
 			_denseLayer.Update();
 		}
 
