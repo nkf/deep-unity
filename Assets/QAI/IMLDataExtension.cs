8f6bd9819e75cadff8609459edb370f9268b56b7
Nikolaj Korsbæk Falsted <nikolajfalsted@gmail.com>
2015-07-28 17:25:35 +0200

    Removed unused class (extension for Encog lib)

--- Assets/QAI/IMLDataExtension.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using Encog.ML.Data;
-
-public static class IMLDataExtension {
-    public static IEnumerable<double> ToEnumerable(this IMLData data) {
-        for (int i = 0; i < data.Count; i++)
-            yield return data[i];
-    }
-}
